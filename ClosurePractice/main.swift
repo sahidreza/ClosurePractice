959ccb3c1076891e173fde05a1713ef37cc79f62
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-24 13:41:12 +0530

    Add more example

--- ClosurePractice/main.swift
+++ ClosurePractice/main.swift
@@ -7,29 +7,86 @@
 
 import Foundation
 
-func printGoodmorningMessage(isMorning:Bool,name:()->String){
-    
-    debugPrint("HEllo")
-    
-    if isMorning{
-        
-        print("Hello Good morning \(name())")
-        
-    }
-    
-}
+//Exampl1 :
 
-func assignName(name:String)-> String {
-    debugPrint("name assigned")
-    return name
-    
-}
+//func calculator(n1:Int , n2:Int,oparation:(Int,Int)->Int) -> Int {
+//
+//    return oparation(n1,n2)
+//}
+//
+//func add(no1:Int, no2:Int) -> Int{
+//    return no1 + no2
+//}
+//
+//let sum = calculator(n1: 5, n2: 6, oparation:{(no1,no2)  in
+//
+//    return no1 + no2
+//
+//} )
+//
+//
+//print(sum)
+//
 
 
 
 
-printGoodmorningMessage(isMorning: false, name: {()->String in
-    
-    assignName(name: "Raju")
-    
-} )
+
+
+
+
+
+
+
+
+
+
+
+
+// Example 2
+
+//func printGoodmorningMessage(isMorning:Bool,name:()->String){
+//
+//    debugPrint("HEllo")
+//
+//    if isMorning{
+//
+//        print("Hello Good morning \(name())")
+//
+//    }
+//
+//}
+//
+//func assignName(name:String)-> String {
+//    debugPrint("name assigned")
+//    return name
+//
+//}
+//
+//
+//
+//
+//printGoodmorningMessage(isMorning: false, name: {()->String in
+//
+//    assignName(name: "Raju")
+//
+//} )
+
+//Example 3
+
+//let arrayList = [5,7,9,7,3]
+//
+//func addOne(v1:Int) ->Int{
+//
+//    return v1 + 1
+//}
+//
+//let result = arrayList.map({(no1:Int)->Int in
+//
+//    return no1 + 1
+//
+//})
+//print(result)
+
+
+
