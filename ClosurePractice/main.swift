1c7982da7868ca892e455a168fb83a035c3db9bc
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-24 12:17:13 +0530

    Initial commit

--- ClosurePractice/main.swift
+++ ClosurePractice/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func printGoodmorningMessage(isMorning:Bool,name:()->String){
+    
+    debugPrint("HEllo")
+    
+    if isMorning{
+        
+        print("Hello Good morning \(name())")
+        
+    }
+    
+}
 
+func assignName(name:String)-> String {
+    debugPrint("name assigned")
+    return name
+    
+}
+
+
+
+
+printGoodmorningMessage(isMorning: false, name: {()->String in
+    
+    assignName(name: "Raju")
+    
+} )
